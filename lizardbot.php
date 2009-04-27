6c186e37169d664251a00462daae91ecc8267b6f
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2009-04-27 00:58:31 +0000

    Fix SudoKing typo fail, probably which he made in r35.  Up to 6.1.1.4.
    git-svn-id: https://lizardbot.googlecode.com/svn/trunk/lizardbot@41 382b71b6-eab9-11dd-85a8-df16bb8a7048

--- lizardbot.php
+++ lizardbot.php
@@ -54,7 +54,7 @@ echo $c_green;
 |_____||_______| |________||_|      |_| |_|   \__\ |____/
 
 PHP-LizardBot: IRC bot developed by FastLizard4 (who else?) and the LizardBot Development Team
-Version 6.1.1.3b (major.minor.build.revision) BETA
+Version 6.1.1.4b (major.minor.build.revision) BETA
 Licensed under the Creative Commons GNU General Public License 2.0 (GPL)
 For licensing details, contact me or read this page:
 http://creativecommons.org/licenses/GPL/2.0/
@@ -90,7 +90,7 @@ PandoraBot extension courtesy of Ttech (PHP-5 OOP)
 <?php
 //Check for updates
 echo "{$c_yellow}Checking for updates...\r\n";
-$version = "6.1.1.3b";
+$version = "6.1.1.4b";
 $upfp = @fopen('http://lizardwiki.gewt.net/w/index.php?title=LizardBot/Latest&action=raw', 'r');
 $data = @fgets($upfp);
 @fclose($upfp);
@@ -830,17 +830,17 @@ IRCO;
 	}
 
 
-	 if($d[3] == chr(001) . "FINGER" . chr(001)) {
-		 $ctcpcount++;
-		 $target = explode("!",$d[0]);
-		 $target = $target[0];
-		 $data = "NOTICE $target :";
-		 $data .= chr(001);
-		 $data .= "FINGER ". $setCTTCPVersion;
-		 $data .= chr(001);
-		 $data .= "\r\n";
-		 fwrite($ircc,$data);
-		 echo <<<IRCO
+	if($d[3] == chr(001) . "FINGER" . chr(001)) {
+		$ctcpcount++;
+		$target = explode("!",$d[0]);
+		$target = $target[0];
+		$data = "NOTICE $target :";
+		$data .= chr(001);
+		$data .= "FINGER ". $setCTCPVersion;
+		$data .= chr(001);
+		$data .= "\r\n";
+		fwrite($ircc, $data);
+		echo <<<IRCO
 -!- SENT CTCP FINGER REPLY TO $target\n
 IRCO;
 	}
@@ -1087,7 +1087,7 @@ in PHP 5 Procedural.  I work on both Windows and *Nix systems with PHP installed
 	if($d[3] == "{$setTrigger}update" && hasPriv('*')) {
 		$cmdcount++;
 		echo "Checking for updates...\r\n";
-		$version = "6.1.1.3b";
+		$version = "6.1.1.4b";
 		$upfp = @fopen('http://lizardwiki.gewt.net/w/index.php?title=LizardBot/Latest&action=raw', 'r');
 		$data = @fgets($upfp);
 		@fclose($upfp);
